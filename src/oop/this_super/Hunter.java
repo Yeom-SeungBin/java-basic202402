bee92c2b2973a2e16c13c8ccd07425ea727b4756
sseung <duaqkr2@naver.com>
2024-02-23 17:37:53 +0900

    # Java oop (this)

---
+++ src/oop/this_super/Hunter.java
@@ -0,0 +1,11 @@
+package oop.this_super;
+
+public class Hunter extends Player {
+    
+    String pet;
+
+    void characterInfo() {
+        super.characterInfo();
+        System.out.println("# 펫 이름 : " + pet);
+    }
+}
